5e6ee88dfe1322221e58f6546ab7cffa7de84097
karolis-bingelis <33342948+karolis-bingelis@users.noreply.github.com>
2017-11-15 21:26:00 +0100

    all

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,78 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20171113221922) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "articles", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "description"
+    t.text "eur_value"
+    t.text "dk_value"
+    t.text "no_value"
+    t.text "se_value"
+    t.text "ch_value"
+    t.text "pounds_value"
+    t.text "dk_lang"
+    t.text "no_lang"
+    t.text "se_lang"
+    t.text "nl_lang"
+    t.text "de_lang"
+    t.text "fr_lang"
+    t.text "en_lang"
+    t.text "link"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "hotels", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "price"
+    t.string "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "room_categories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "hotel_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hotel_id"], name: "index_room_categories_on_hotel_id"
+  end
+
+  add_foreign_key "comments", "articles"
+  add_foreign_key "room_categories", "hotels"
 end
